5053a667e45fb633506981d6d22dd2755ed1e740
fukumamire <rinka_karyou@hotmail.com>
2024-03-17 11:18:16 +0900

    first commit

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('auth/register.css') }}">
+@endsection
+
+@section('content')
+<div class="register__content">
+  <div class="register-form__heading">
+    <h2>会員登録</h2>
+  </div>
+  <form class="form" action="/register" method="post">
+    @csrf
+    <div class="form__group">
+      <input type="text" name="name" placeholder="名前" >
+    </div>
+    <div class="form__group">
+        <input type="email" name="email" placeholder="メールアドレス" >
+    </div>
+    <div class="form__group">
+      <input type="password" name="password" placeholder="パスワード">
+    </div>
+    <div class="form__group">
+      <input type="password" name="password_confirmation" placeholder="確認用パスワード" >
+    </div>
+    <div class="form__button">
+      <button type="submit" class="register-button">会員登録</button>
+    </div>
+  </form>
+  <div class="login-link">
+      <p>アカウントをお持ちの方はこちらから</p>
+      <a href="/login">ログイン</a>
+  </div>
+</div>
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atte</title>
+  <link rel="stylesheet" href="{{ asset('auth/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('auth/common.css') }}">
+  @yield('css')
+</head>
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <h1 class="header__heading">Atte</h1>  
+    </div>
+    @yield('link')
+  </header>
+
+  <main>
+    @yield('content')
+  </main>
+  
+  <footer class="footer">
+    <div class="footer__inner">
+      <h3>Atte,inc</h3>
+    </div>
+  </footer>
+</body>
